b1aacffb75c279fe05a4437a209a68f1c0ca55b7
Keernom <keernom.gdev@gmail.com>
2024-04-17 12:56:11 +0300

    feat: base entitiy created

---
+++ TaskSolution/Entities/Adstactions/CommonObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Adstactions
+{
+    abstract class CommonObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public byte[] Image { get; set; }
+
+        public CommonObject()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
