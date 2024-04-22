4832b67470147ad63c42b04234fb514bb6edb0b5
Keernom <keernom.gdev@gmail.com>
2024-04-22 17:10:12 +0300

    feat: base abstract service added

---
+++ TaskSolution/TaskManagerApi/Services/Abstracts/AbstractService.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskManagerApi.Services.Abstracts
+{
+    public abstract class AbstractService
+    {
+        protected bool DoAction(Action action)
+        {
+            if (action == null) return false;
+
+            try
+            {
+                action();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
