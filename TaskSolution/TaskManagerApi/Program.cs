4129a85b4eba5c8b1f6ebd08a5e1f979311c9578
Keernom <keernom.gdev@gmail.com>
2024-04-23 19:33:04 +0300

    feat: newtonsoft added

--- TaskSolution/TaskManagerApi/Program.cs
+++ TaskSolution/TaskManagerApi/Program.cs
@@ -56,6 +56,7 @@ namespace TaskManagerApi
             builder.Services.AddTransient<AccountService>();
             builder.Services.AddTransient<ProjectService>();
             builder.Services.AddTransient<DeskService>();
+            builder.Services.AddControllers().AddNewtonsoftJson();
 
             var app = builder.Build();
 
