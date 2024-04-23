d9ec08c634401b12a5ae351ebe694361a244746d
Keernom <keernom.gdev@gmail.com>
2024-04-23 19:32:09 +0300

    feat: desk constructors added

--- TaskSolution/Entities/Models/Desk.cs
+++ TaskSolution/Entities/Models/Desk.cs
@@ -1,8 +1,11 @@
 ﻿using Entities.Abstractions;
+using Entities.DTOs;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Entities.Models
@@ -16,5 +19,33 @@ namespace Entities.Models
         public int ProjectId { get; set; }
         public Project Project { get; set; }
         public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
+
+        public Desk() { }
+
+        public Desk(DeskDTO deskDTO) : base(deskDTO) 
+        {
+            IsPrivate = deskDTO.IsPrivate;
+            AdminId = deskDTO.AdminId;
+            ProjectId = deskDTO.ProjectId;
+
+            if (deskDTO.Columns.Any())
+                Columns = JsonConvert.SerializeObject(deskDTO.Columns);
+        }
+
+        public DeskDTO ToDto()
+        {
+            return new DeskDTO()
+            {
+                Id = Id,
+                Name = Name,
+                Description = Description,
+                CreatedDate = CreatedDate,
+                Image = Image,
+                AdminId = AdminId,
+                IsPrivate = IsPrivate,
+                Columns = JsonConvert.DeserializeObject<string[]>(Columns),
+                ProjectId = ProjectId,
+            };
+        }
     }
 }
