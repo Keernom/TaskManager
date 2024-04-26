23e7727f2690b5cc1e7aa6314ac5e3e430c74143
Keernom <keernom.gdev@gmail.com>
2024-04-26 19:17:19 +0300

    feat: unit-test project added

---
+++ TaskSolution/TaskManagerWPFTests/Services/UserRequestServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaskManagerWPF.Services.Tests
+{
+    [TestClass()]
+    public class UserRequestServiceTests
+    {
+        [TestMethod()]
+        public void GetTokenTest()
+        {
+            var token = new UserRequestService().GetToken("admin", "qwerty123");
+            Console.WriteLine(token.Access_Token);
+            Console.WriteLine(token.Username);
+            Assert.IsNotNull(token.Access_Token);
+        }
+    }
+}
