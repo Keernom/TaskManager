c7c114a75c4670a6e4c7cf5be879be80f3f6f398
Keernom <keernom.gdev@gmail.com>
2024-04-26 21:13:29 +0300

    feat: others methods to user service added

--- TaskSolution/TaskManagerWPF/Services/UserRequestService.cs
+++ TaskSolution/TaskManagerWPF/Services/UserRequestService.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using Entities.DTOs;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManagerWPF.Models;
@@ -15,34 +17,96 @@ namespace TaskManagerWPF.Services
     public class UserRequestService
     {
         private const string HOST = "https://localhost:7090/api";
+        private string _userController = $"{HOST}/users";
 
-        private T GetDataByUrl<T>(string url, string username, string password)
+        private T GetDataByUrl<T>(string url, AuthToken token, string username = null, string password = null)
         {
             HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(url);
-            webRequest.Method = "POST";
+            webRequest.Method = "GET";
 
             if (username != null && password != null)
             {
+                webRequest.Method = "POST";
                 string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
                 webRequest.Headers.Add("Authorization", "Basic " + encoded);
-
-                HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
-
-                using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                {
-                    string responseText = sr.ReadToEnd();
-                    T res = JsonConvert.DeserializeObject<T>(responseText);
-                    return res;
-                }
             }
 
-            return default(T);
+            if (token.Access_Token != null)
+            {
+                webRequest.Headers.Add("Authorization", token.Access_Token);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+
+            using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                string responseText = sr.ReadToEnd();
+                T res = JsonConvert.DeserializeObject<T>(responseText);
+                return res;
+            }
+        }
+
+        private async Task<HttpStatusCode> DoActionWithDataByUrl(HttpMethod method, string url, AuthToken authToken, string data = null)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken.Access_Token);
+
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            result = method.Method switch
+            {
+                "Post" => await client.PostAsync(url, content),
+                "Patch" => await client.PatchAsync(url, content),
+                "Delete" => await client.DeleteAsync(url)
+            };
+
+            return result.StatusCode;
         }
 
         public AuthToken GetToken(string username, string password)
         {
             string url = $"{HOST}/account/token";
-            return GetDataByUrl<AuthToken>(url, username, password);
+            return GetDataByUrl<AuthToken>(url, new AuthToken() ,username, password);
+        }
+
+        public async Task<HttpStatusCode> CreateUser(AuthToken token, UserDTO user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = await DoActionWithDataByUrl(HttpMethod.Post, _userController, token, userJson);
+
+            return result;
+        }
+
+        public List<UserDTO> GetUsers(AuthToken token)
+        {
+            var users = GetDataByUrl<List<UserDTO>>(_userController, token);
+            return users;
+        }
+
+        public async Task<HttpStatusCode> DeleteUser(AuthToken token, int userId)
+        {
+            var result = await DoActionWithDataByUrl(HttpMethod.Delete, _userController + $"/{userId}", token);
+
+            return result;
+        }
+
+        public async Task<HttpStatusCode> CreateMultipleUser(AuthToken token, List<UserDTO> users)
+        {
+            string usersJson = JsonConvert.SerializeObject(users);
+            var result = await DoActionWithDataByUrl(HttpMethod.Post, _userController + "/all", token, usersJson);
+
+            return result;
+        }
+
+        public async Task<HttpStatusCode> UpdateUser(AuthToken token, UserDTO user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = await DoActionWithDataByUrl(HttpMethod.Patch, _userController + $"/{user.Id}", token, userJson);
+
+            return result;
         }
     }
 }
