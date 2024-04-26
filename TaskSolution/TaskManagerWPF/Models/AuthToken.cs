4212873f494f32dd8f143b5f7203a9e48ff40bb1
Keernom <keernom.gdev@gmail.com>
2024-04-26 19:16:33 +0300

    feat: token entity added

---
+++ TaskSolution/TaskManagerWPF/Models/AuthToken.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerWPF.Models
+{
+    public class AuthToken
+    {
+        public string Access_Token { get; set; }
+        public string Username { get; set; }
+    }
+}
