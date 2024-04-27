8d358347c797401b7d44e907cb53512bc1e5ac66
Keernom <keernom.gdev@gmail.com>
2024-04-27 18:17:45 +0300

    feat: common request service added

--- TaskSolution/TaskManagerWPF/Services/UserRequestService.cs
+++ TaskSolution/TaskManagerWPF/Services/UserRequestService.cs
@@ -10,62 +10,11 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskManagerWPF.Models;
 
-using static System.Net.WebRequestMethods;
-
 namespace TaskManagerWPF.Services
 {
-    public class UserRequestService
-    {
-        private const string HOST = "https://localhost:7090/api";
+    public class UserRequestService : CommonRequestService
+    {   
         private string _userController = $"{HOST}/users";
-
-        private (T value, HttpStatusCode code) GetDataByUrl<T>(string url, AuthToken token, string username = null, string password = null)
-        {
-            HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(url);
-            webRequest.Method = "GET";
-
-            if (username != null && password != null)
-            {
-                webRequest.Method = "POST";
-                string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
-                webRequest.Headers.Add("Authorization", "Basic " + encoded);
-            }
-
-            if (token.Access_Token != null)
-            {
-                webRequest.Headers.Add("Authorization", "Bearer " + token.Access_Token);
-            }
-
-            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
-
-            using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-            {
-                string responseText = sr.ReadToEnd();
-                T res = JsonConvert.DeserializeObject<T>(responseText);
-
-                return (res, response.StatusCode);
-            }
-        }
-
-        private async Task<HttpStatusCode> DoActionWithDataByUrl(HttpMethod method, string url, AuthToken authToken, string data = "")
-        {
-            HttpResponseMessage result = new HttpResponseMessage();
-            HttpClient client = new HttpClient();
-
-            client.DefaultRequestHeaders.Authorization =
-                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authToken.Access_Token);
-
-            var content = new StringContent(data, Encoding.UTF8, "application/json");
-
-            result = method.Method switch
-            {
-                "POST" => await client.PostAsync(url, content),
-                "PATCH" => await client.PatchAsync(url, content),
-                "DELETE" => await client.DeleteAsync(url)
-            };
-
-            return result.StatusCode;
-        }
 
         public (AuthToken token, HttpStatusCode code) GetToken(string username, string password)
         {
