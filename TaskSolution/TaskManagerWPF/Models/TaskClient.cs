26c07ef20347fadd22e0f6bbb1e207c75bbe758a
Keernom <keernom.gdev@gmail.com>
2024-05-04 01:55:13 +0300

    feat: task client class added

---
+++ TaskSolution/TaskManagerWPF/Models/TaskClient.cs
@@ -0,0 +1,36 @@
+﻿using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using TaskManagerWPF.Models.Extensions;
+
+namespace TaskManagerWPF.Models
+{
+    public class TaskClient
+    {
+        public TaskDTO Model { get; private set; }
+        public TaskClient(TaskDTO model)
+        {
+            Model = model;
+        }
+
+        public UserDTO Creator { get; set; }
+        public UserDTO Executor { get; set; }
+        public BitmapImage Image
+        {
+            get
+            {
+                return Model.LoadImage();
+            }
+        }
+        public bool IsHaveFile
+        {
+            get => Model?.File != null;
+        }
+
+    }
+}
