c6ac2689b2fac13437e230854738eee207598cea
Keernom <keernom.gdev@gmail.com>
2024-05-05 20:00:03 +0300

    feat: common dto client added

---
+++ TaskSolution/TaskManagerWPF/Models/CommonDtoClient.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using TaskManagerWPF.Models.Extensions;
+
+namespace TaskManagerWPF.Models
+{
+    public class CommonDtoClient<T> where T : CommonDTO
+    {
+        public T Model { get; set; }
+
+        public CommonDtoClient(T model)
+        {
+            Model = model;
+        }
+
+        public BitmapImage Image
+        {
+            get
+            {
+                return Model?.LoadImage();
+            }
+        }
+    }
+}
